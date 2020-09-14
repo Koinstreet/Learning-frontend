8fa05054b78af9c644423fd93564e5d80b188233
Gideon Nnalue <gideon@ximedialabs.com>
2020-09-14 01:28:22 +0100

    push build

---
+++ build/precache-manifest.1b03642683d7267a64ec1aa44faa9c28.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1026c3900898c6016adc4d35dd720b7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dbd109462cfed7558ef",
+    "url": "/static/css/2.d9901f9e.chunk.css"
+  },
+  {
+    "revision": "c596bb64effaa06e5826",
+    "url": "/static/css/main.748a100b.chunk.css"
+  },
+  {
+    "revision": "0dbd109462cfed7558ef",
+    "url": "/static/js/2.a440ab8f.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.a440ab8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c596bb64effaa06e5826",
+    "url": "/static/js/main.cf7cb4d0.chunk.js"
+  },
+  {
+    "revision": "4ef08f4bca5e277c892b",
+    "url": "/static/js/runtime-main.52ecdf84.js"
+  },
+  {
+    "revision": "973e50ee3f923ce72f127c4b2ae90405",
+    "url": "/static/media/cryptimage.973e50ee.png"
+  },
+  {
+    "revision": "fde509346f15d3ba91979f889bf3be43",
+    "url": "/static/media/face.fde50934.png"
+  },
+  {
+    "revision": "a69620f53aa8840068e119535e04426e",
+    "url": "/static/media/facebook-white-icon.a69620f5.svg"
+  },
+  {
+    "revision": "6383dbbcac77c8ab94c3e978b9d357b1",
+    "url": "/static/media/flag.6383dbbc.svg"
+  },
+  {
+    "revision": "486ab8bf33ce29d4c5838ca3bf063121",
+    "url": "/static/media/instagram-white-icon.486ab8bf.svg"
+  },
+  {
+    "revision": "d134214ab8b9c540d5b6914b06f9fc0f",
+    "url": "/static/media/logo.d134214a.png"
+  },
+  {
+    "revision": "71d53fa9d305182719a146b937a730de",
+    "url": "/static/media/logoblue.71d53fa9.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d3fbae8291e5da107fac9b491c71ac84",
+    "url": "/static/media/slide1.d3fbae82.png"
+  },
+  {
+    "revision": "c176473e068a87f8ab10273e80675527",
+    "url": "/static/media/twitter-white-icon.c176473e.svg"
+  }
+]);
